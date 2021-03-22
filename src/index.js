240c45c4b3accc9e231536e93808fde7232fe8db
Andrey Shemet <an-shem@ukr.net>
2021-03-22 04:21:55 +0200

    доработал логику, вынес функцию в отдельный файл

--- src/index.js
+++ src/index.js
@@ -1,60 +1,29 @@
 import './styls.css';
 import fetchCountries from './js/fetchCountries';
-
-import templateLIst from './templates/template-list.hbs';
-import templateData from './templates/template-data.hbs';
-
-import { error, defaultModules } from '@pnotify/core/dist/PNotify.js';
-import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
-import '@pnotify/core/dist/BrightTheme.css';
-import '@pnotify/core/dist/PNotify.css';
-
-defaultModules.set(PNotifyMobile, {});
+import { arrayHandler, error } from './js/array-handler';
 
 const debounce = require('lodash.debounce');
 
 // -----------------------------------------------------
-
-const boxDataaRef = document.querySelector('.search-display');
+const boxDataRef = document.querySelector('.search-display');
 const inputRef = document.querySelector('.input-search');
 
 inputRef.addEventListener('input', debounce(onInput, 500));
 
 function onInput(e) {
   const searchQuery = e.target.value.trim();
-  boxDataaRef.innerHTML = '';
+  boxDataRef.innerHTML = '';
   if (searchQuery === '') return;
   fetchCountries(searchQuery).then(arrayCountries => {
-    arrayHandler(arrayCountries);
+    if (arrayCountries.status) {
+      error({
+        title: 'ОШИБКА!',
+        text: 'Нет такой страны',
+        animateSpeed: 'normal',
+        delay: 2000,
+      });
+    } else {
+      arrayHandler(arrayCountries, boxDataRef);
+    }
   });
-}
-
-function arrayHandler(array) {
-  if (array.length > 10) {
-    error({
-      type: 'error',
-      title: 'ОШИБКА!',
-      text: 'Сделайте запрос более специфичным.',
-      animateSpeed: 'normal',
-      delay: 1500,
-      mouseReset: true,
-    });
-  }
-
-  if (array.length >= 2 && array.length <= 10) {
-    const markupBox = array.map(({ name }) => name);
-    boxDataaRef.innerHTML = templateLIst(markupBox);
-  }
-  if (array.length === 1) {
-    const markupBox = array.map(
-      ({ name, capital, population, languages, flag }) => ({
-        name,
-        capital,
-        population,
-        languages: languages.map(item => item.name),
-        flag,
-      }),
-    );
-    boxDataaRef.innerHTML = templateData(...markupBox);
-  }
 }
---
+++ src/js/array-handler.js
@@ -0,0 +1,40 @@
+import templateLIst from '../templates/template-list.hbs';
+import templateData from '../templates/template-data.hbs';
+
+import { error, defaultModules } from '@pnotify/core/dist/PNotify.js';
+import * as PNotifyMobile from '@pnotify/mobile/dist/PNotifyMobile.js';
+import '@pnotify/core/dist/BrightTheme.css';
+import '@pnotify/core/dist/PNotify.css';
+
+defaultModules.set(PNotifyMobile, {});
+
+// ---------------------------------------------------
+function arrayHandler(array, boxDataRef) {
+  if (array.length > 10) {
+    error({
+      title: 'ОШИБКА!',
+      text: 'Сделайте запрос более специфичным.',
+      animateSpeed: 'normal',
+      delay: 2000,
+    });
+  }
+
+  if (array.length >= 2 && array.length <= 10) {
+    const markupBox = array.map(({ name }) => name);
+    boxDataRef.innerHTML = templateLIst(markupBox);
+  }
+  if (array.length === 1) {
+    const markupBox = array.map(
+      ({ name, capital, population, languages, flag }) => ({
+        name,
+        capital,
+        population,
+        languages: languages.map(item => item.name),
+        flag,
+      }),
+    );
+    boxDataRef.innerHTML = templateData(...markupBox);
+  }
+}
+
+export { arrayHandler, error };
